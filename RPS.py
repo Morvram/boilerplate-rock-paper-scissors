924e9d74f71f15669d1c90a49a38a99dd3fbc412
Morvram <JRMhere@protonmail.ch>
2023-08-04 19:24:52 +0000

    strategy: adapt to consistent opponent plays

--- RPS.py
+++ RPS.py
@@ -4,7 +4,16 @@ def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
     guess = "R"
-    if len(opponent_history) > 2:
+    #If the opponent keeps playing the same play again and again, play what beats that
+    if len(opponent_history) > 2 and opponent_history[-1] == opponent_history[-2] == opponent_history[-3]:
+      repeat = opponent_history[-1]
+      if repeat == "R":
+        guess = "P"
+      elif repeat == "P":
+        guess = "S"
+      else:
+        guess = "R"
+    elif len(opponent_history) > 2: #This method beats Kris
         guess = opponent_history[-2]
 
     return guess
