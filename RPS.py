acdf1a544081146e7217499f12f8e5764e277681
Morvram <JRMhere@protonmail.ch>
2023-08-07 21:00:39 +0000

    attempted other solution.

--- RPS.py
+++ RPS.py
@@ -6,4 +6,16 @@ def player(prev_play, opponent_history=[]):
   #Implement the markov matrix method here:
 
   guess = "R"  #Default guess from boilerplate. Plays rock every time.
+
+  # 1. Counting R/P/S most used by opponent
+  # opponent_history = ["P","P","P","S","S","R"]
+  # dori-min solution, idk how well this works but attempting it for reference.
+  countdict = {
+    "R": opponent_history.count("R"),
+    "P": opponent_history.count("P"),
+    "S": opponent_history.count("S")
+  }
+  countdict = dict(sorted(countdict.items(), key=lambda x: x[1], reverse=True))
+  guess = [key for key in countdict.keys()][0]
+
   return guess
